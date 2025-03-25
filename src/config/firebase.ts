92542119aac2f27b060a9cabf80eedfd9c226e29
MARCOS RIVAS KYOGURO <marcos.rivkyo@usal.es>
2025-03-25 18:47:58 +0100

    Actualización 25/03 Canvas y Storage integrado

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,17 +3,15 @@ import { getAuth, Auth } from "firebase/auth";
 import { getFirestore, Firestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyDOJoSj4BPVEORJDgIN5H-TJN9JvXQdEnA",
-    authDomain: "hcc-ai.firebaseapp.com",
-    projectId: "hcc-ai",
-    storageBucket: "hcc-ai.firebasestorage.app",
-    messagingSenderId: "822292218390",
-    appId: "1:822292218390:web:5dffe0efc7b45a382a6b37"
-  };
-  
-  
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY, 
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
 const app = initializeApp(firebaseConfig);
 
 const auth: Auth = getAuth(app);
@@ -21,5 +19,3 @@ const db: Firestore = getFirestore(app);
 const storage = getStorage(app);
 
 export { app, auth, db, storage };
-
-
